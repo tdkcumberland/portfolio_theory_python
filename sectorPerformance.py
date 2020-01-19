fb6d7b2826e8df54491fd8764ec6062e94618d85
Timothy Cumberland <tdkcumberland@gmail.com>
2020-01-19 18:06:01 -0500

    removed sectorPerformance.py, irrelevant for now

--- sectorPerformance.py
+++
@@ -1,9 +0,0 @@
-import pandas_datareader.data as pddata
-
-def get_sector_perf(apiKey):
-    """
-    Wrapper to retrieve sector performance from Alpha Advantage with a default API Key
-    """
-    api_key ='QSMUWQIPLV31UH4Y' if apiKey =='' else apiKey
-    df = pddata.get_sector_performance_av(api_key)
-    return df
